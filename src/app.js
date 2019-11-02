39fc797b1ee7564ecb4d00efe52eab64b76e36b2
Avula Chandra Shekar <avulachandrashekar12@gmail.com>
2019-11-02 18:25:55 +0800

    New Changes

--- src/app.js
+++ src/app.js
@@ -7,9 +7,26 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
+const accountData = fs.readFileSync(
+    path.join(__dirname + "/json/accounts.json"), {
+        encoding: "UTF8"
+    });
+
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join(__dirname + '/json/users.json'), {
+    encoding: "UTF8"
+})
+
+const users = JSON.parse(userData);
+
+console.log(users);
+console.log(accounts);
+
 app.get('/', (req, res) => {
     res.render('index', {
-        title: 'Index'
+        title: "Account Summary",
+        accounts
     })
 })
 
